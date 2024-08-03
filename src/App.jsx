87aa06bf918830899f7d149209aa29683906b90d
ayanjis <ayancdas@gmail.com>
2024-08-03 12:45:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Todo from './Components/Todo'
+
+function App() {
+
+  return (
+    <div>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Todo.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState, useRef } from 'react'
+import './CSS/Todo.css'
+import TodoItems from './TodoItems'
+
+let count = 0
+function Todo() {
+
+  const [todos, setTodos] = useState([])
+  const inputRef = useRef(null)
+
+  const add = () => {
+    setTodos([...todos, { no: count++, text: inputRef.current.value, display: '' }])
+    inputRef.current.value = ''
+    localStorage.setItem('todos_count', count)
+  }
+
+  useEffect(() => {
+    setTodos(JSON.parse(localStorage.getItem('todos')))
+    count = localStorage.getItem('todos_count')
+  }, [])
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(todos)
+      localStorage.setItem('todos', JSON.stringify(todos))
+    }, 100);
+  }, [todos])
+
+  return (
+    <div>
+      <div className="todo">
+        <div className="todo-header">To-Do List</div>
+        <div className="todo-add">
+          <input className='todo-input' ref={inputRef} type="text" placeholder='Add your task' />
+          <div onClick={() => { add() }} className="todo-add-btn">ADD</div>
+        </div>
+        <div className="todo-list">
+          {
+            todos.map((item, index) => {
+              return <TodoItems key={index} setTodos={setTodos} no={item.no} display={item.display} text={item.text} />
+            })
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Todo

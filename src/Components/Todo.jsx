14e16c831b09f4569193a8e0b9887c89ac8a79d1
ayanjis <ayancdas@gmail.com>
2024-08-03 22:49:07 +0530

    updates

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,17 +1,24 @@
 import React, { useEffect, useState, useRef } from 'react'
 import './CSS/Todo.css'
 import TodoItems from './TodoItems'
+import UpdatePopup from './UpdatePopup'
 
 let count = 0
+
 function Todo() {
+  const [showUpdatePopup, setShowUpdatePopup] = useState(false)
+  const [popupContent, setPopupContent] = useState({})
 
   const [todos, setTodos] = useState([])
   const inputRef = useRef(null)
 
   const add = () => {
     setTodos([...todos, { no: count++, text: inputRef.current.value, display: '' }])
-    inputRef.current.value = ''
-    localStorage.setItem('todos_count', count)
+    if (inputRef.current.value == '') {
+      localStorage.removeItem('todos_count', count)
+    } else {
+      localStorage.setItem('todos_count', count)
+    }
   }
 
   useEffect(() => {
@@ -26,22 +33,39 @@ function Todo() {
     }, 100);
   }, [todos])
 
+  const revTodos = [].concat(todos).reverse()
+
   return (
     <div>
       <div className="todo">
         <div className="todo-header">To-Do List</div>
         <div className="todo-add">
-          <input className='todo-input' ref={inputRef} type="text" placeholder='Add your task' />
-          <div onClick={() => { add() }} className="todo-add-btn">ADD</div>
+        {/* <form className="todo-add" onSubmit={handleSubmit(submit)}> */}
+          {/* <input className='todo-input' ref={inputRef} type="text" name={name} placeholder='Add your task' { ...register(name, { required: true }) }/> */}
+          <input className='todo-input' ref={inputRef} type="text" placeholder='Add your task'/>
+          <button onClick={() => { add() }} className="todo-add-btn">ADD</button>
         </div>
         <div className="todo-list">
           {
-            todos.map((item, index) => {
-              return <TodoItems key={index} setTodos={setTodos} no={item.no} display={item.display} text={item.text} />
+            revTodos.map((item, index) => {
+              return <TodoItems key={index} 
+              setTodos={setTodos} no={item.no} 
+              setShowUpdatePopup={setShowUpdatePopup} 
+              setPopupContent={setPopupContent}
+              display={item.display} 
+              text={item.text} />
             })
+            
           }
         </div>
       </div>
+
+      {
+        showUpdatePopup && <UpdatePopup
+          setShowUpdatePopup={setShowUpdatePopup}
+          popupContent={popupContent}
+        />
+      }
     </div>
   )
 }
--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -4,7 +4,20 @@ import tick from './Assets/tick.png'
 import not_tick from './Assets/not_tick.png'
 import cross from './Assets/cross.png'
 
-function TodoItems({no, display, text, setTodos}) {
+function TodoItems({no, display, text, setTodos, setShowUpdatePopup, setPopupContent }) {
+
+  const clickToUpdateUser = (no) => {
+    setPopupContent()
+    setShowUpdatePopup(true)
+    // if (condition) {
+      
+    // } else {
+      
+    // }
+    let data = JSON.parse(localStorage.getItem('todos'))
+    data = data.filter((todo) => todo.no !== no)
+    setTodos(data)
+  }
 
   const toggle = (no) => {
     let data = JSON.parse(localStorage.getItem('todos'))
@@ -34,7 +47,8 @@ function TodoItems({no, display, text, setTodos}) {
         {display === '' ? <img src={not_tick} alt="" /> : <img src={tick} alt="" />}
         <div className="todoitems-text">{text}</div>
       </div>
-      <img className='todoitems-cross-icon' src={cross} alt="" onClick={() => {deleteTodo(no)}}/>
+      {/* <img className='todoitems-cross-icon' src={cross} alt="" onClick={() => {deleteTodo(no)}}/> */}
+      <img className='todoitems-cross-icon' src={cross} alt="" onClick={() => clickToUpdateUser(no)}/>
     </div>
   )
 }
